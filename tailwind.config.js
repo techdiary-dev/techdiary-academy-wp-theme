2d79954f52fd92028b512b2ce8a35357668014d2
kingrayhan <rayhan095@gmail.com>
2021-09-04 15:38:23 +0600

    init

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./**/*.php"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#8A9CFE",
+        secondary: "#FDF9F3",
+        typography: "#0A083B",
+      },
+      fontFamily: {
+        "operator-mono": ["Operator Mono"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+      spacing: {
+        "50vh": "50vh",
+        "60vh": "60vh",
+        "70vh": "70vh",
+        "80vh": "80vh",
+        "90vh": "90vh",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
---
+++ template-parts/hero-area.php
@@ -0,0 +1,28 @@
+<div class="grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
+    <div>
+        <p class=" text-lg text-typography">Hello, We Are Tech Diary</p>
+        <h2 class=" leading-10 capitalize md:text-5xl text-4xl font-operator-mono font-semibold text-typography mb-4">
+            we helps you to <br> <span class=" text-primary">easier your</span> <br>
+            learning
+        </h2>
+
+        <a href="#" class="!hidden md:!inline-flex button">
+            <span>Get started</span>
+            <svg width="21" height="16" viewBox="0 0 21 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M20.7071 8.70711C21.0976 8.31658 21.0976 7.68342 20.7071 7.29289L14.3431 0.928932C13.9526 0.538408 13.3195 0.538408 12.9289 0.928932C12.5384 1.31946 12.5384 1.95262 12.9289 2.34315L18.5858 8L12.9289 13.6569C12.5384 14.0474 12.5384 14.6805 12.9289 15.0711C13.3195 15.4616 13.9526 15.4616 14.3431 15.0711L20.7071 8.70711ZM0 9L20 9V7L0 7L0 9Z" fill="white" />
+            </svg>
+        </a>
+    </div>
+
+    <div class=" self-start md:self-center flex flex-col items-center space-y-10">
+        <img src="<?php echo get_template_directory_uri() . '/assets/images/hero-vector.png' ?>" />
+
+        <a href="#" class="md:!hidden button">
+            <span>Get started</span>
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12.6667 10L11.7267 9.06001L8.66671 12.1133V1.33334H7.33337V12.1133L4.27337 9.05334L3.33337 10L8.00004 14.6667L12.6667 10Z" fill="white" />
+            </svg>
+        </a>
+
+    </div>
+</div>
